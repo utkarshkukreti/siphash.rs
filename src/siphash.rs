8346468f6b65cde27de988df699ba60da3533a55
Utkarsh Kukreti <utkarshkukreti@gmail.com>
2014-06-29 20:51:14 +0530

    Add `SipHasher::new{,_with_keys}` methods to create new `SipHasher`s.

--- src/siphash.rs
+++ src/siphash.rs
@@ -5,3 +5,16 @@ pub struct SipHasher {
     k0: u64,
     k1: u64
 }
+
+impl SipHasher {
+    pub fn new() -> SipHasher {
+        SipHasher::new_with_keys(0, 0)
+    }
+
+    pub fn new_with_keys(k0: u64, k1: u64) -> SipHasher {
+        SipHasher {
+            k0: k0,
+            k1: k1
+        }
+    }
+}
