80d09221a702c5bbbd58e195ef30b6e9ef6ba3f6
Utkarsh Kukreti <utkarshkukreti@gmail.com>
2014-06-29 20:54:27 +0530

    Add boilerplate to make `cargo test` work.

--- src/siphash.rs
+++ src/siphash.rs
@@ -1,5 +1,10 @@
 #![crate_id = "siphash"]
+#![feature(globs, phase)]
 #![no_std]
+
+// For tests.
+#[cfg(test)] #[phase(plugin, link)] extern crate std;
+#[cfg(test)] extern crate native;
 
 pub struct SipHasher {
     k0: u64,
@@ -16,5 +21,13 @@ impl SipHasher {
             k0: k0,
             k1: k1
         }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    #[test]
+    fn test_1_plus_1() {
+        assert_eq!(1u + 1, 2);
     }
 }
